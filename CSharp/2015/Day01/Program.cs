becf3c482c864c2bf0f5610e9ebccc3911c93ebe
Jaleel <jeremikress@gmail.com>
2024-12-05 11:44:03 +0200

    complete day 01 in csharp

--- CSharp/2015/Day01/Program.cs
+++ CSharp/2015/Day01/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace Day01;
+
+
+class Program
+{
+    private const string InputFilePath = "../../../input/input.txt";
+    public static string GetFileContents(string filePath)
+    {
+        string readContents = "";
+        using (StreamReader streamReader = new StreamReader(filePath, Encoding.UTF8))
+        {
+            readContents = streamReader.ReadToEnd();
+        }
+
+        return readContents;
+    }
+
+    static int PartOne(string input)
+    {
+        int currentFloor = 0;
+       
+        
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case '(':
+                    currentFloor += 1;
+                    break;
+                case ')':
+                    currentFloor -= 1;
+                    break;
+            }
+        }
+
+        return currentFloor;
+    }
+
+    static int PartTwo(string input)
+    {
+        int currentFloor = 0;
+        int i = 0;
+        
+        foreach (char c in input)
+        {
+            i++;
+            
+            switch (c)
+            {
+                case '(':
+                    currentFloor += 1;
+                    break;
+                case ')':
+                    currentFloor -= 1;
+                    break;
+            }
+
+            if (currentFloor == -1)
+            {
+                return i;
+            }
+
+            
+
+        }
+
+        return i;
+
+    }
+    
+    
+    
+    static void Main(string[] args)
+    {
+        try
+        {
+            string fileContents = GetFileContents(InputFilePath);
+
+            Console.WriteLine($"Part One: {PartOne(fileContents)}");
+            Console.WriteLine($"Part Two: {PartTwo(fileContents)}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
---
+++ CSharp/Shared/Util.cs
@@ -0,0 +1,9 @@
+﻿namespace Utilities;
+
+public class Util
+{
+    public static class IEnumerableExtensions {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(IEnumerable<T> self)       
+            => self.Select((item, index) => (item, index));
+    }
+}
