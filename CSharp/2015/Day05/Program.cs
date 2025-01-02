128621ab1705dc18369141e4f982810f84e4ada3
Jaleel-VS <jeremikress@example.com>
2025-01-02 20:26:50 +0200

    start day05

---
+++ CSharp/2015/Day05/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day05;
+
+class Program
+{
+    private const string InputFilePath = "../../../input/input.txt";
+    
+    public static string[] GetFileInput => File.ReadAllLines(InputFilePath);
+
+    public static int PartOne(string[] strings)
+    {
+        var 
+    }
+    
+    public static void Main(string[] args)
+    {
+        
+    }
+}
