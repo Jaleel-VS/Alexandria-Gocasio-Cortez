7c3961ede7018b17e1acc36adc5ef7888ca34317
Jaleel-VS <jeremikress@gmail.com>
2025-09-25 17:50:51 +0200

    feat: solve part 2

--- aoc2015/day01/main.go
+++ aoc2015/day01/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
+var part = flag.Int("part", 1, "run part 1 or 2")
+
 func main() {
-	data, err := readFile("input.txt")
+	flag.Parse()
+
+	lines, err := readLines("input.txt")
 	if err != nil {
 		log.Fatalf("could not read file: %v", err)
 	}
+	switch *part {
+	case 1:
+		answer := solvePart1(lines)
+		fmt.Println("Part 1 Answer:", answer)
+	case 2:
+		answer := solvePart2(lines)
+		fmt.Println("Part 2 Answer:", answer)
+	default:
+		log.Fatalf("invalid part number: %d", *part)
+	}
+}
 
+func solvePart1(lines []string) int {
 	floorLevel := 0
-
-	for _, char := range data {
+	for _, char := range lines[0] {
 		switch char {
 		case '(':
 			floorLevel++
@@ -22,16 +39,44 @@ func main() {
 			floorLevel--
 		}
 	}
+	return floorLevel
+}
 
-	fmt.Printf("Santa is on floor %d\n", floorLevel)
+func solvePart2(lines []string) int {
+	floorLevel := 0
+
+	for i, char := range lines[0] {
+		switch char {
+		case '(':
+			floorLevel++
+		case ')':
+			floorLevel--
+		}
+
+		if floorLevel == -1 {
+			return i + 1
+		}
+	}
+
+	return 0
 }
 
 
 
-func readFile(path string) (string, error) {
-    data, err := os.ReadFile(path)
-    if err != nil {
-        return "", err
-    }
-    return string(data), nil
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
 }
